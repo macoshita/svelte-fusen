6939dcfc3695e57dda11b97815e71f2de4acc839
macoshita <macoshita@users.noreply.github.com>
2021-02-17 22:46:00 +0900

    Add Delete Button

--- src/components/atoms/Fusen.svelte
+++ src/components/atoms/Fusen.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
-  import { fly } from "svelte/transition";
+  import { fly, fade } from "svelte/transition";
   import { pannable } from "../../actions/pannable";
+  import { fusens } from "../../stores";
   import type { FusenType } from "../../stores"; // https://github.com/pyoner/svelte-typescript/issues/23
 
   export let data: FusenType;
@@ -12,6 +13,19 @@
       y: data.y + detail.dy,
     };
   }
+
+  let isShowDeleteButton = false;
+
+  function showDeleteButton() {
+    isShowDeleteButton = true;
+  }
+  function hideDeleteButton() {
+    isShowDeleteButton = false;
+  }
+
+  function deleteFusen() {
+    fusens.update((v) => v.filter((fusen) => fusen.id !== data.id));
+  }
 </script>
 
 <div
@@ -21,21 +35,47 @@ left: {data.x}px;
 top: {data.y}px;
 background-color: {data.color};
 "
+  transition:fly={{ y: -100, duration: 200 }}
   use:pannable
   on:panmove={handlePanMove}
-  in:fly={{ y: -100, duration: 200 }}
-  role="textbox"
-  contenteditable
-  bind:textContent={data.description}
-/>
+  on:mouseenter={showDeleteButton}
+  on:mouseleave={hideDeleteButton}
+>
+  <div
+    class="content"
+    role="textbox"
+    contenteditable
+    bind:textContent={data.description}
+  />
+  {#if isShowDeleteButton}
+    <ion-icon
+      transition:fade
+      class="close"
+      name="close-circle-outline"
+      on:click={deleteFusen}
+    />
+  {/if}
+</div>
 
 <style>
   .fusen {
     position: absolute;
+    box-shadow: 0 5px 5px rgba(0, 0, 0, 0.1);
+    box-sizing: border-box;
+  }
+
+  .close {
+    position: absolute;
+    right: -16px;
+    top: -16px;
+    font-size: 32px;
+    border-radius: 50%;
+    background-color: white;
+  }
+
+  .content {
+    padding: 16px;
     min-width: 160px;
     min-height: 80px;
-    box-shadow: 0 5px 5px rgba(0, 0, 0, 0.1);
-    padding: 16px;
-    box-sizing: border-box;
   }
 </style>
